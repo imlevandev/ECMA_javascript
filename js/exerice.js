0cf5f4bc70f814fc70713550fff37700f6d1649f
imlevandev <nguyennro001a@gmail.com>
2023-12-19 23:20:34 -0500

    labs

---
+++ js/exerice.js
@@ -0,0 +1,15 @@
+var name1 = " nguyenn";
+var tuoi1 = " 20";
+
+function sayhello() {
+  console.log(`Im ${name1}. im ${tuoi1}`);
+}
+
+var name2 = "sully";
+var tuoi2 = "21";
+
+var sayhello2 = function () {
+  console.log(`im ${name2}, im ${tuoi2}`);
+};
+sayhello();
+sayhello2();
